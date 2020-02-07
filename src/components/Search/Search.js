bca78a1003b49740c7115ea77d0fc7c0fd838b99
Micah Fukazawa <micahfukazawa@gmail.com>
2020-02-07 20:33:54 +0900

    [Add] fromyear styled label

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const FromYear = styled.label`
+    font-size: 1.5em;
+    font-width: bold;
+`
+
 const Search = () => {
   return (
     <div>
-        <label>From Year</label><input type="number" min="2014" max="2019"></input>
+        <FromYear>From Year</FromYear><input placeholder="2014" type="number" min="2014" max="2019"></input>
         <label>To Year</label><input type="number" min="2014" max="2019"></input>
         <label>From Quarter</label><input type="number" min="1" max="4"></input>
         <label>To Quarter</label><input type="number" min="1" max="4"></input>
