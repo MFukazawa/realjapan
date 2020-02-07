f9de7151053dec0e2d4142953c7bac118ceeafa9
Micah Fukazawa <micahfukazawa@gmail.com>
2020-02-07 15:04:47 +0900

    [Add] Navigation component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <div className="App">
         <Navigation />
-        <Logo />
-        <SearchFields />
+        {/* {<Logo />
+        <SearchFields />} */}
       </div>
     )
   }
