77896552cc7decf488ccafed12438831e4469bb4
Micah Fukazawa <micahfukazawa@gmail.com>
2020-02-10 11:14:06 +0900

    [Add] search field styles, select inputs

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,21 +1,95 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const FromYear = styled.label`
-    font-size: 1.5em;
-    font-width: bold;
+const Wrapper = styled.div` 
+  padding: 0 10%;
+`
+
+const SearchLabel = styled.label`
+  font-size: 1em;
+`;
+
+const SearchItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1em 0;
+`;
+
+const SearchSelect= styled.select`
+  text-align-last: center;
+  text-align: center;
+   -ms-text-align-last: center;
+   -moz-text-align-last: center;
 `
 
 const Search = () => {
   return (
-    <div>
-        <FromYear>From Year</FromYear><input placeholder="2014" type="number" min="2014" max="2019"></input>
-        <label>To Year</label><input type="number" min="2014" max="2019"></input>
-        <label>From Quarter</label><input type="number" min="1" max="4"></input>
-        <label>To Quarter</label><input type="number" min="1" max="4"></input>
-        <label>Area</label><input></input>
-    </div> 
+    <Wrapper>
+      <SearchItem>
+        <SearchLabel htmlFor="FromYear">From Year</SearchLabel>
+        <SearchSelect name="FromYear" id="FromYear">
+          <option className="search-option" value="">--Please choose an option--</option>
+          <option value="2014">2014</option>
+          <option value="2015">2015</option>
+          <option value="2016">2016</option>
+          <option value="2017">2017</option>
+          <option value="2018">2018</option>
+          <option value="2019">2019</option>
+        </SearchSelect>
+      </SearchItem>
+
+      <SearchItem>
+        <SearchLabel>To Year</SearchLabel>
+        <select name="ToYear" id="ToYear">
+          <option value="">--Please choose an option--</option>
+          <option value="2014">2014</option>
+          <option value="2015">2015</option>
+          <option value="2016">2016</option>
+          <option value="2017">2017</option>
+          <option value="2018">2018</option>
+          <option value="2019">2019</option>
+        </select>
+      </SearchItem>
+
+      <SearchItem>
+        <SearchLabel>From Quarter</SearchLabel>
+        <select name="FromQuarter" id="FromQuarter">
+          <option value="">--Please choose an option--</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+      </SearchItem>
+
+      <SearchItem>
+        <SearchLabel>To Quarter</SearchLabel>
+        <select name="ToQuarter" id="ToQuarter">
+          <option value="">--Please choose an option--</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+      </SearchItem>
+
+      <SearchItem>
+        <SearchLabel>Area</SearchLabel>
+        <select></select>
+      </SearchItem>
+
+      <SearchItem>
+        <SearchLabel>City</SearchLabel>
+        <select></select>
+      </SearchItem>
+
+      <SearchItem>
+        <SearchLabel>Station</SearchLabel>
+        <select></select>
+      </SearchItem>
+    </Wrapper>
   );
-}
+};
 
 export default Search;
