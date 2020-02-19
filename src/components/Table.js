6f8587a53b06ecbbe97e2b2679999dbefbea7e30
Micah Fukazawa <micahfukazawa@gmail.com>
2020-02-19 11:31:54 +0900

    [Add] district search box

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { useTable } from "react-table";
+import React, { useState } from "react";
+import { useTable, useFilters } from "react-table";
 
 export default function Table({ columns, data }) {
   const {
@@ -7,35 +7,57 @@ export default function Table({ columns, data }) {
     getTableBodyProps, // table body props from react-table
     headerGroups, // headerGroups if your table have groupings
     rows, // rows for the table based on the data passed
-    prepareRow // Prepare the row (this function need to called for each row before getting the row props)
-  } = useTable({
-    columns,
-    data
-  });
+    prepareRow, // Prepare the row (this function need to called for each row before getting the row props)
+    setFilter
+  } = useTable(
+    {
+      columns,
+      data
+    },
+    useFilters
+  );
+
+  const [filterInput, setFilterInput] = useState("");
+
+  const handleFilterChange = event => {
+    const value = event.target.value || "";
+    setFilter("DistrictName", value);
+    console.log(value);
+    setFilterInput(value);
+  };
 
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-              })}
+    <>
+      <input
+        value={filterInput}
+        onChange={handleFilterChange}
+        placeholder={"Search District"}
+      />
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 }
